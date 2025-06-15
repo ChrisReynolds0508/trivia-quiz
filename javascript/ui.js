395ca944945ed1e8fb0a9fc4cb8c5f0106e442fa
Chrisreynolds0508 <Chris.reynolds0508@gmail.com>
2025-06-15 16:49:37 +1000

    dynamically created html elements and exported them to my main.js file

---
+++ javascript/ui.js
@@ -0,0 +1,50 @@
+
+const container = document.createElement('div'); // app container 
+container.classList.add('container');
+document.body.appendChild(container);
+
+const heading = document.createElement('h1');
+heading.classList.add('heading');
+heading.textContent = 'Trivia Quiz';
+container.appendChild(heading);
+
+const questionDiv = document.createElement('div');  
+questionDiv.classList.add('questions');
+container.appendChild(questionDiv);
+
+const score = document.createElement('div'); 
+score.classList.add('score');
+container.appendChild(score);
+
+const beginButton = document.createElement('button'); 
+beginButton.classList.add('beginButton');
+beginButton.textContent = 'Begin Quiz';
+container.appendChild(beginButton);
+
+const nextButton = document.createElement('button'); 
+nextButton.classList.add('nextButton');
+nextButton.textContent = 'Next Question';
+container.appendChild(nextButton);
+
+const previousQuestion = document.createElement('button'); 
+previousQuestion.classList.add('previousQuestion');
+previousQuestion.textContent = 'Previous Question';
+container.appendChild(previousQuestion);
+
+const timer = document.createElement('div'); 
+timer.classList.add('timer');
+timer.textContent = 'Timer';
+container.appendChild(timer);
+
+const elements = {
+    container,
+    heading,
+    questionDiv,
+    score,
+    beginButton,
+    nextButton,
+    previousQuestion,
+    timer
+}
+
+export default elements;
